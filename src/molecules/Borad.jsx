a352005e72c1d65747e99aedd44805be640c7f21
吉本光輝 <>
2018-05-03 13:06:34 +0900

    change borad file

--- src/molecules/Borad.jsx
+++ src/molecules/Borad.jsx
@@ -14,22 +14,13 @@ class Borad extends React.Component {
   render() {
     return (
       <div className="board-row">
-        {this.renderSquare(0)}
-        {this.renderSquare(1)}
-        {this.renderSquare(2)}
-        {this.renderSquare(3)}
-        {this.renderSquare(4)}
-        {this.renderSquare(5)}
-        {this.renderSquare(6)}
-        {this.renderSquare(7)}
-        {this.renderSquare(8)}
-        {this.renderSquare(9)}
-        {this.renderSquare(10)}
-        {this.renderSquare(11)}
-        {this.renderSquare(12)}
-        {this.renderSquare(13)}
-        {this.renderSquare(14)}
-        {this.renderSquare(15)}
+        {[...Array(16).keys()].map((i) => {
+          return(
+            <div>
+              {this.renderSquare(i)}
+            </div>
+          )
+        })}
       </div>
     )
   }
